c8a8535db44e576af501cf89376a7ed27a26c4ec
Jennifer Evans <jenevans3ten@gmail.com>
2016-04-27 14:43:00 -0400

    Week4 initial commit files

---
+++ Week4_Practice_01302016/Week4_Practice_01302016/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Week4_Practice_01302016
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string a = "word1";
+            string b = "word2";
+            string c = "word3";
+            string d = "longword1";
+            string e = "longword2";
+            string f = "longword3";
+            Console.WriteLine($"{a,-15}, {b,-15}, {c,-15}.");
+            Console.WriteLine($"{d,15}, {e,15}, {f,15}.");
+
+
+        }
+    }
+}
